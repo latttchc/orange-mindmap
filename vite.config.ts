3211e171615b65cf6b5ed02480ee4aa02d795d2c
latttchc <iam.yuk2882@gmail.com>
2025-03-29 18:32:31 +0900

    GitHub Pagesへデプロイする設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import { fileURLToPath } from "url";
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
